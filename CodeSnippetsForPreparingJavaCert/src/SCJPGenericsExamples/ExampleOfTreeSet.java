5bd94eff0099e19cccae8f970713b7705eabb3cf
aorlovsky <aorlovsky@PC22145544.cc-college.gecs.loc>
2017-02-24 08:37:43 +0100

    Very bad code from SCJP book!!!

---
+++ CodeSnippetsForPreparingJavaCert/src/SCJPGenericsExamples/ExampleOfTreeSet.java
@@ -0,0 +1,24 @@
+package SCJPGenericsExamples;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ExampleOfTreeSet {
+
+    public static void main(String[] args) {
+        before();
+    }
+    
+    
+    public static void before() {
+        Set set = new TreeSet();
+        set.add("2");
+        set.add(3);
+        set.add("1");
+        Iterator it = set.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+}
