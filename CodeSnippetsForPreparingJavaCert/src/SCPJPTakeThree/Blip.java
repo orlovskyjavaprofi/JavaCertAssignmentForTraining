619b4c13428d7852cda0355a054769d29e9ed0a3
aorlovsky <aorlovsky@PC22145544.cc-college.gecs.loc>
2017-03-03 12:29:57 +0100

    Very bad code from VCE Queition pool!!!

---
+++ CodeSnippetsForPreparingJavaCert/src/SCPJPTakeThree/Blip.java
@@ -0,0 +1,5 @@
+package SCPJPTakeThree;
+//Here is only one example taken from VCE pool
+public class Blip {
+
+}
---
+++ CodeSnippetsForPreparingJavaCert/src/SCPJPTakeThree/Vert.java
@@ -0,0 +1,5 @@
+package SCPJPTakeThree;
+//Here is only one example taken from VCE pool
+public class Vert extends Blip{
+  // insert code here
+}
